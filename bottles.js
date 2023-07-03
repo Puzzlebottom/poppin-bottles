bbfe39aec1ca032b2fe672bc75faa7ee432a3b68
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-03 09:56:58 -0400

    completed solution

--- bottles.js
+++ bottles.js
@@ -18,14 +18,19 @@ const process = require("process");
 
 const totalBottles = (resources) => {
   const pricePerBottle = 2;
+  const capConversion = 4;
+  const emptyConversion = 2;
+
   let bottles = 0;
+  let redeemedForCaps = 0;
+  let redeemedForEmpties = 0;
 
   let { cash, caps, empties } = resources;
 
-  if (cash >= pricePerBottle || caps >= 4 || empties >= 2) {
-    bottles += Math.floor(caps / 4);
-    bottles += Math.floor(empties / 2);
-    bottles += Math.floor(cash / pricePerBottle);
+  if (cash >= pricePerBottle || caps >= capConversion || empties >= emptyConversion) {
+    redeemedForCaps += Math.floor(caps / 4);
+    redeemedForEmpties += Math.floor(empties / 2);
+    bottles += Math.floor(cash / pricePerBottle) + redeemedForCaps + redeemedForEmpties;
 
     cash = cash % 2;
     caps = caps % 4;
@@ -33,12 +38,18 @@ const totalBottles = (resources) => {
 
     let recycling = totalBottles({ cash: cash, caps: bottles + caps, empties: bottles + empties });
 
+    //accumulation
     bottles += recycling.totalBottles;
+    redeemedForCaps += recycling.redeemedForCaps;
+    redeemedForEmpties += recycling.redeemedForEmpties;
+
+    //dissipation
     caps = recycling.caps;
     empties = recycling.empties;
+
   }
 
-  return { totalBottles: bottles, caps: caps, empties: empties };
+  return { totalBottles: bottles, caps: caps, empties: empties, redeemedForEmpties: redeemedForEmpties, redeemedForCaps: redeemedForCaps, };
 };
 
 
@@ -52,3 +63,4 @@ let result = totalBottles({ cash: investment, caps: 0, empties: 0 });
 console.log(`TOTAL BOTTLES: ${result.totalBottles}`);
 console.log(`REMAINING BOTTLES: ${result.empties}`);
 console.log(`REMAINING CAPS: ${result.caps}`);
+console.log(`TOTAL EARNED:\n  BOTTLES: ${result.redeemedForEmpties}\n  CAPS: ${result.redeemedForCaps}`);
