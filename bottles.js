de4d28b43173931ac2c1f0453fc4991fbcf17509
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-03 09:44:42 -0400

    implements functionality to return remainder of empties and caps

--- bottles.js
+++ bottles.js
@@ -1,19 +1,44 @@
 const process = require("process");
 
 
-const totalBottles = (cash, caps = 0, empties = 0) => {
-  let pricePerBottle = 2;
+// const totalBottles = (cash, caps = 0, empties = 0) => {
+//   let pricePerBottle = 2;
+//   let bottles = 0;
+
+//   if (cash >= pricePerBottle || caps >= 4 || empties >= 2) {
+//     bottles += Math.floor(cash / pricePerBottle);
+//     bottles += Math.floor(caps / 4);
+//     bottles += Math.floor(empties / 2);
+
+//     bottles += totalBottles(cash % 2, bottles + (caps % 4), bottles + (empties % 2));
+//   }
+
+//   return bottles;
+// };
+
+const totalBottles = (resources) => {
+  const pricePerBottle = 2;
   let bottles = 0;
 
+  let { cash, caps, empties } = resources;
+
   if (cash >= pricePerBottle || caps >= 4 || empties >= 2) {
-    bottles += Math.floor(cash / pricePerBottle);
     bottles += Math.floor(caps / 4);
     bottles += Math.floor(empties / 2);
+    bottles += Math.floor(cash / pricePerBottle);
 
-    bottles += totalBottles(cash % 2, bottles + (caps % 4), bottles + (empties % 2));
+    cash = cash % 2;
+    caps = caps % 4;
+    empties = empties % 2;
+
+    let recycling = totalBottles({ cash: cash, caps: bottles + caps, empties: bottles + empties });
+
+    bottles += recycling.totalBottles;
+    caps = recycling.caps;
+    empties = recycling.empties;
   }
 
-  return bottles;
+  return { totalBottles: bottles, caps: caps, empties: empties };
 };
 
 
@@ -22,4 +47,8 @@ if (input === undefined) return console.log("Not enough arguments!");
 if (input.length > 3) return console.log("Too many arguments!");
 
 let investment = input[2];
-console.log(`You're headed home with ${totalBottles(investment, 0, 0)} bottles!`);
+let result = totalBottles({ cash: investment, caps: 0, empties: 0 });
+
+console.log(`TOTAL BOTTLES: ${result.totalBottles}`);
+console.log(`REMAINING BOTTLES: ${result.empties}`);
+console.log(`REMAINING CAPS: ${result.caps}`);
